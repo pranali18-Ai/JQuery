d08032d447ad6ef07c13fb0811f5fb9c6ac46265
Pranali Patil <patilpranali0018@gmail.com>
2023-09-15 11:15:47 +0530

    14 sept 2023

---
+++ Js/myjQuery.js
@@ -0,0 +1,29 @@
+ //1. Element Selector
+    $(document).ready(function () {
+    // //   //All JQuery code
+      console.log("We are using JQuery");
+    // //   // $('selector').action()
+    // //   // $('p').click();
+    // //   // $('p').click(function() {
+    // //   //     console.log("You clicked p!");
+    // //   // }); //do this when we click on p
+
+    // //   $("button").click(function () {
+    // //     console.log("button clicked!!", this);
+    // //     // $('button').hide();
+    // //     // $(this).hide();
+    // //     // $('#id').hide();   //Id
+    // //     // $('.class').hide    //class
+    // //   });
+
+    // //   // 2.Id selector
+    // //   // $("#2").click();
+
+    // //   //3. Class selector
+    // //   // $(".odd").click();
+
+    // //   //other selectors
+    // //   // $("button.odd").click(); //for specific class button
+    // //   $("button:first").click(); //first button
+    // //   $("*").click(); //clicks on all the elements
+    });
