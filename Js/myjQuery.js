9a58e7fdd9767059f95b37d78ac5396d4b5c4ada
Pranali Patil <patilpranali0018@gmail.com>
2023-09-20 19:24:21 +0530

    20 sept 2023

--- Js/myjQuery.js
+++ Js/myjQuery.js
@@ -1,29 +1,67 @@
- //1. Element Selector
-    $(document).ready(function () {
-    // //   //All JQuery code
-      console.log("We are using JQuery");
-    // //   // $('selector').action()
-    // //   // $('p').click();
-    // //   // $('p').click(function() {
-    // //   //     console.log("You clicked p!");
-    // //   // }); //do this when we click on p
+//1. Element Selector
+$(document).ready(function () {
+  console.log("I am in the new file");
 
-    // //   $("button").click(function () {
-    // //     console.log("button clicked!!", this);
-    // //     // $('button').hide();
-    // //     // $(this).hide();
-    // //     // $('#id').hide();   //Id
-    // //     // $('.class').hide    //class
-    // //   });
+  // Output a message to indicate that jQuery is being used
+  console.log("We are using jQuery");
 
-    // //   // 2.Id selector
-    // //   // $("#2").click();
+  // // Select all <button> elements and attach a click event handler
+  // $("button").click(function () {
+  //   console.log("Button clicked !", this);
+  // });
 
-    // //   //3. Class selector
-    // //   // $(".odd").click();
+  // Select all <button> elements and attach a click event handler
+  //   $("button").click(function () {
+  //     console.log("button clicked!!");
+  //     $(this).hide(); // Hide the clicked button
+  //     $("#id").hide(); // Hide an element with the ID 'id'
+  //     $(".class").hide(); // Hide all elements with the class 'class'
+  //   });
+  // });
 
-    // //   //other selectors
-    // //   // $("button.odd").click(); //for specific class button
-    // //   $("button:first").click(); //first button
-    // //   $("*").click(); //clicks on all the elements
-    });
+  //  $("button").click(function () {
+  //  console.log("button clicked!!", this);
+  //  $('button').hide();
+  //  $(this).hide();
+  //  $('#id').hide();   //Id
+  //  $('.class').hide    //class
+  //  });
+
+  // Types of Selectors
+  // 1. Element selector
+  // $('button').click();
+  // //   // 2.Id selector
+  // //   // $("#2").click();
+
+  // //   //3. Class selector
+  // //   // $(".odd").click();
+
+  // //   //other selectors
+  // //   // $("button.odd").click(); //for specific class button
+  // //   $("button:first").click(); //first button
+  // //   $("*").click(); //clicks on all the elements
+
+  // ***Events***
+  // 1. Mouse Event : Click, dbClick, mousecenter, mouseleave
+  // 2. Keyboard Event: Keypress, keydown, MediaKeyStatusMap
+  // 3. form Event: Submit, Change, focus, blur
+  // 4. document/windows Event: load, resize, scroll, unload
+
+  //dblclick
+  // $("button").dblclick(function () {
+  //   console.log("Button double clicked !", this);
+  // });
+
+  // //mouseenter
+  // $("button").mouseenter(function () {
+  //   console.log("mouse entered !", this);
+  // });
+
+  $("button").hover(function () {
+    console.log(" you hovered on;", this);
+  });
+
+  $("button").click(function () {
+    console.log("Button clicked !", this);
+  });
+});
